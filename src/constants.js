3c57ecff1a3365b6a78471899769ecf955d23576
skshreek2 <103485010+skshreek2@users.noreply.github.com>
2026-04-25 15:11:18 +0530

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,6 @@
+export const UPI_TRANSFER = "UPI Transfer";
+export const NEFT_TRANSFER = "NEFT Transfer";
+export const ADD_PAYEE = "Add NewPayee";
+export const MANAGE_PAYEE = "Manage Payee";
+export const IMPS = "IMPS Trasnfer";
+export const RTGS = "RTGS Transfer";
